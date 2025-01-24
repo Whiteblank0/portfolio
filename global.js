2c4da04f53265d1d580b9051ce9d19784da5c8da
Yaoxi Li <yal149@ucsd.edu>
2025-01-24 02:13:36 -0800

    Update

--- global.js
+++ global.js
@@ -5,14 +5,38 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// Get all nav links
-const navLinks = $$("nav a");
+// // Get all nav links
+// const navLinks = $$("nav a");
 
-// Find the current link and add a 'current' class
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+// // Find the current link and add a 'current' class
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname
+// );
 
-if (currentLink) {
-  currentLink.classList.add('current');
+// if (currentLink) {
+//   currentLink.classList.add('current');
+// }
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'cv/', title: 'CV' },
+]
+
+// Detect if we are on the home page
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  
+  // Adjust the URL for non-home pages
+  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+  // Create link and add it to nav
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
