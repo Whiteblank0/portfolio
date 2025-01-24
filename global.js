623cc9acd561d27894ae63fdb0df77ff4adcf1d6
Yaoxi Li <yal149@ucsd.edu>
2025-01-23 23:53:33 -0800

    Step 2

--- global.js
+++ global.js
@@ -1,5 +1,18 @@
 console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
+  // Select multiple elements within the given context
   return Array.from(context.querySelectorAll(selector));
+}
+
+// Get all nav links
+const navLinks = $$("nav a");
+
+// Find the current link and add a 'current' class
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+if (currentLink) {
+  currentLink.classList.add('current');
 }
