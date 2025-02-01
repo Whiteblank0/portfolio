c11d862cc5d79ef00188302838f41ac6196ca0c9
Yaoxi Li <yal149@ucsd.edu>
2025-01-31 23:19:41 -0800

    Update global.js

--- global.js
+++ global.js
@@ -211,5 +211,5 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
 
 export async function fetchGitHubData(username) {
   // fetchJSON returns a Promise that resolves to the parsed JSON data.
-  return fetchJSON(`https://api.github.com/users/Whiteblank0`);
+  return fetchJSON(`https://api.github.com/users/${username}`);
 }
