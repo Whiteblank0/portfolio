9dbd96eef802425edc776a12f465613d45323b9b
Yaoxi Li <yal149@ucsd.edu>
2025-02-15 16:24:42 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -10,6 +10,9 @@ async function loadData() {
     date: new Date(row.date + 'T00:00' + row.timezone),
     datetime: new Date(row.datetime),
   }));
+
+  processCommits();
+  console.log(commits);
 }
 
 function processCommits() {
@@ -22,25 +25,22 @@ function processCommits() {
       // Build a commit summary object
       let ret = {
         id: commit,
-        url: 'https://github.com/YOUR_REPO/commit/' + commit, // Replace YOUR_REPO accordingly
+        url: 'https://github.com/Whiteblank0/portfolio/commit/' + commit, // Replace YOUR_REPO accordingly
         author,
         date,
         time,
         timezone,
         datetime,
-        // Calculate hour fraction for time-of-day analysis (e.g., 2:30 PM becomes 14.5)
         hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
-        // Total number of lines modified in this commit
         totalLines: lines.length,
       };
 
       // Add the original line data as a hidden property using Object.defineProperty.
-      // Setting enumerable: false ensures it doesn't clutter up the output when printed.
       Object.defineProperty(ret, 'lines', {
         value: lines,
         writable: true,     // Allow changes if needed
         configurable: true, // Allow the property to be reconfigured later
-        enumerable: false   // Hide it from standard enumeration (e.g., console.log)
+        enumerable: false   // Hide it from standard enumeration
       });
 
       return ret;
